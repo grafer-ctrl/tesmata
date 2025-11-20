b5f5089ea5dcd557331862f8837c1ee6cab8c9aa
grafer-ctrl <umarsaid.radiografer@gmail.com>
2025-11-20 20:22:25 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,19 @@
-const CACHE_NAME = 'ishihara-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-    // Tambahkan 'icon.png' jika Anda punya file lokalnya
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './readme.html',
+    './eagleeye.html',
+    './Ishihara.html',
+    './SnellenQuiz.html',
+    './TumblingE.html',
+    './NeuroFocus.html',
+    './ShadowHunter.html',
+    './SchulteTable.html',
+    './KineticVision.html',
+    './ChromaMind.html',
+    './EagleSearch.html',
+    './E-Swipe.html',  // Sepertinya ada file ini juga di gambar
+    './manifest.json'
+    // './icon-192.png', // Hapus baris ini dulu jika Anda belum upload gambar ikon
+    // './icon-512.png'  // Hapus baris ini dulu jika Anda belum upload gambar ikon
 ];
-
-// Install Service Worker
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(ASSETS))
-    );
-});
-
-// Fetch Data (Gunakan Cache jika offline)
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                return response || fetch(event.request);
-            })
-    );
-});
